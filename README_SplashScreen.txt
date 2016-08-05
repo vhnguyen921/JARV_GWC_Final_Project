8b76b2fd3ae015f50b05f6e661bc87c548590f6e
bluelightning13 <mehiatt@wpi.edu>
2016-08-05 08:07:36 -0400

    WorkingSplash

---
+++ README_SplashScreen.txt
@@ -0,0 +1,17 @@
+ReadMe.txt
+
+To use this example that I have modified:
+
+1. Download it.
+2. Extract the file to the Visual Studio Projects folder under Documents -> Visual Studio -> Projects.
+	Full file path of directory C:\Users\Girls Who Code\Documents\Visual Studio\Projects
+3.Once extracted, open Visual Studio. Go to File -> Open -> Project
+4.Open the SplashExample App
+5.Once completely open, try to run this code. 
+	Use Android_Accelerated option (only if you do not have Android_Accelerated, use 7" KitKat) 
+	
+If there are problems let me know. The main.axml file is your main page - not your splash page. Replace the 
+splash_logo.png under the Resources -> Drawable folder with your desired image. 
+
+I made a second layout called options. It is not connected to anything yet, but if you need another page
+for something, do it on the options.axml. I can help with the navigation between pages if needed. 
---
+++ SplashScreen/SplashActivity/obj/Debug/android/src/md56d0f808042ab50794ff70c3e2cde8186/Activity1.java
@@ -0,0 +1,47 @@
+package md56d0f808042ab50794ff70c3e2cde8186;
+
+
+public class Activity1
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("com.xamarin.sample.splashscreen.Activity1, SplashScreen, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Activity1.class, __md_methods);
+	}
+
+
+	public Activity1 () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == Activity1.class)
+			mono.android.TypeManager.Activate ("com.xamarin.sample.splashscreen.Activity1, SplashScreen, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}

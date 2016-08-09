1fe0eda3aa32828df27d1632546f0603bd8274c7
wangjenny56 <xjennyx3@gmail.com>
2016-08-09 11:00:57 -0400

    Activity1

--- Updated_Splash_And_Camera/SplashScreen/SplashActivity/Activity1.cs
+++ Updated_Splash_And_Camera/SplashScreen/SplashActivity/Activity1.cs
@@ -12,7 +12,7 @@ using Android.Provider;
 using Java.IO;
 using Environment = Android.OS.Environment;
 using Uri = Android.Net.Uri;
-
+          //CHEAT APP!!!!!!!!///**********************************************
 namespace com.xamarin.sample.splashscreen
 {
     public static class App
@@ -27,7 +27,7 @@ namespace com.xamarin.sample.splashscreen
     {
         private ImageView imageView;
         static readonly string TAG = "X:" + typeof (Activity1).Name;
-        
+
 
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
@@ -47,6 +47,7 @@ namespace com.xamarin.sample.splashscreen
             int height = Resources.DisplayMetrics.HeightPixels;
             int width = Resources.DisplayMetrics.WidthPixels;// _imageView.Height;
             App.bitmap = App._file.Path.LoadAndResizeBitmap(width, height);
+        }
 
 
             //if (App.bitmap != null)
@@ -59,11 +60,87 @@ namespace com.xamarin.sample.splashscreen
             // Dispose of the Java side bitmap.
             //GC.Collect();
 
-            //PUT CLARIFAI CODE HERE
+            //CLARIFAI CODE HERE
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("NSFW Tagger, powered by Clarifai.com");
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
 
-            //
+                Console.WriteLine("Potentially NSFW URL: {Added for debug}");
+                // string remoteUri = Console.ReadLine();
+                // string vikingDude = "https://i.imgur.com/lnzsgv9.jpg";
+                //string nudeWoman = "https://i.imgur.com/aJXq544.jpg";
+
+                string test1 = "http://www.mortonarb.org/files/EVENT_Oak-Collection.jpg";
+                string modelURL = "http://api.clarifai.com/v1/tag/?&url=" + test1 + "&access_token=2V76B0VAYy3bVBMwbooE2QBrb6gf89";
+                // make a webclient 
+                WebClient myWebClient = new WebClient();
+                // download & save in a databuffer)
+                byte[] myDataBuffer = myWebClient.DownloadData(modelURL);
+                // full data buffer -> string
+                string download = Encoding.ASCII.GetString(myDataBuffer);
+                // DEBUG 
+                // Console.WriteLine(download); 
+
+                ClarNSFW(download);
+
+            }
+            public static void ClarNSFW(string download)
+            {
+
+                int iop = download.IndexOf("classes");
+                int iof = download.IndexOf("docid_str");
+                string strn = download.Substring(iop, iof - iop);
+
+                //Enable for dbg 
+                Console.WriteLine("Parsed Text: ");
+                Console.WriteLine(" ");
+                //Console.WriteLine(strn);
+                Console.WriteLine(strn.Split(' ')[1]);
+
+
+                int iof2 = download.IndexOf("nsfw");
+                int iop2 = download.IndexOf("sfw");
+                // dbg 
+                // Console.WriteLine("{0}, {1}", iof2, iop2);
+
+
+                string[] numbers = Regex.Split(download, @"\D+");
+
+                int beforeDeciOne = int.Parse(numbers[1]);
+                int beforeDeciTwo = int.Parse(numbers[3]);
+                double afterDeciOne = double.Parse(numbers[2]);
+                double afterDeciTwo = double.Parse(numbers[4]);
+
+                if (iof2 > iop2)
+                {
+                    // Console.BackgroundColor = ConsoleColor.White;
+                    //Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Image has been deemed SAFE FOR WORK");
+                    Console.WriteLine("     probability: 0 . {0} %", afterDeciTwo);
+                }
+                else if (iop2 > iof2)
+                {
+                    //Console.BackgroundColor = ConsoleColor.White;
+                    // Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Image has been deemed NOT SAFE FOR WORK");
+                    Console.WriteLine("     probability: 0 . {0} %", afterDeciOne);
+                }
+                else
+                {
+                    Console.WriteLine("Couldnt tell...");
+                }
+            }
         }
-        protected override void OnCreate(Bundle bundle)
+    }
+
+
+}
+//End of Clarifai code.
+protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
@@ -108,5 +185,4 @@ namespace com.xamarin.sample.splashscreen
 
             StartActivityForResult(intent, 0);
         }
-    }
-}
+    
